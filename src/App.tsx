369203afd35dab7cfee5a3fc48164d60233fcad1
chocopie777 <egoran99@gmail.com>
2023-11-01 14:40:07 +0500

    is done adding and saving pizzas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./models/Pizza";
 
 
-function App() {
-  return (
-    <div className="App">
-      New App!
-    </div>
-  );
+const App: FC = () => {
+    const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+    const addPizza = (newPizza: Pizza) => {
+        setPizzasList([...pizzasList, newPizza]);
+    }
+
+    console.log('pizzasList', pizzasList);
+
+    return (
+        <div className="App">
+            <div className="wrap">
+                <span className='heading'>Наша пиццерия</span>
+                <AddPizzaForm addPizza={addPizza}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
